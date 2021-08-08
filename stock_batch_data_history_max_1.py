0540b8b11899b58e875ce7799fbb407da2be072a
weipanchang <weipanchang@hotmail.com>
2021-08-08 15:05:20 -0700

    change class to method

--- stock_batch_data_history_max_1.py
+++ stock_batch_data_history_max_1.py
@@ -38,9 +38,9 @@ class Logger(object):
         global downloadPath
         global stock
         today = date.today()
-        d1 = today.strftime("%m%d%Y")
+        #d1 = today.strftime("%m%d%Y")
         self.terminal = sys.stdout
-        self.log = open(downloadPath +"\\Summary_Report_"+ d1 + ".txt" , "a+")
+        self.log = open(downloadPath +"\\Summary_Report_"+ today.strftime("%m%d%Y") + ".txt" , "a+")
 
     def write(self, message):
         self.terminal.write(message)
@@ -52,7 +52,7 @@ class Logger(object):
         #you might want to specify some extra behavior here.
         pass
 
-class get_historical_data():
+class get_data:
 
     #def __init__(self, stock_name, startDate, endDate, downloadPath):
     def __init__(self, stock_or_fund):
@@ -62,171 +62,149 @@ class get_historical_data():
         print ("")
 #        print ("Processing " + self.stock_name.upper() +" stock data")
         self.stock_or_fund = stock_or_fund
-        delay = 0
-        currentDateTime = datetime.datetime.now()
-        date = currentDateTime.date()
+        self.delay = 0
+        self.currentDateTime = datetime.datetime.now()
+        self.date = self.currentDateTime.date()
         
-        profile = webdriver.FirefoxProfile()
-        profile.set_preference("browser.download.folderList", 2)
-        profile.set_preference("browser.download.manager.showWhenStarting", False)
-        profile.set_preference("browser.download.dir", downloadPath)
-        profile.set_preference("browser.helperApps.neverAsk.openFile", "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
-        profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
-        profile.set_preference("browser.helperApps.alwaysAsk.force", False)
-        profile.set_preference("browser.download.manager.alertOnEXEOpen", False)
-        profile.set_preference("browser.download.manager.focusWhenStarting", False)
-        profile.set_preference("browser.download.manager.useWindow", False)
-        profile.set_preference("browser.download.manager.showAlertOnComplete", False)
-        profile.set_preference("browser.download.manager.closeWhenDone", False)
-        profile.set_preference("browser.cache.disk.enable", False)
-        profile.set_preference("browser.cache.memory.enable", False)
-        profile.set_preference("browser.cache.offline.enable", False)
-        profile.set_preference("network.http.use-cache", False)
-        desiredCapabilities = DesiredCapabilities.FIREFOX.copy()
-        desiredCapabilities['firefox_profile'] = profile.encoded
-        options = Options()
-#        options.add_argument("--headless")
+        self.profile = webdriver.FirefoxProfile()
+        self.profile.set_preference("browser.download.folderList", 2)
+        self.profile.set_preference("browser.download.manager.showWhenStarting", False)
+        self.profile.set_preference("browser.download.dir", downloadPath)
+        self.profile.set_preference("browser.helperApps.neverAsk.openFile", "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+        self.profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+        self.profile.set_preference("browser.helperApps.alwaysAsk.force", False)
+        self.profile.set_preference("browser.download.manager.alertOnEXEOpen", False)
+        self.profile.set_preference("browser.download.manager.focusWhenStarting", False)
+        self.profile.set_preference("browser.download.manager.useWindow", False)
+        self.profile.set_preference("browser.download.manager.showAlertOnComplete", False)
+        self.profile.set_preference("browser.download.manager.closeWhenDone", False)
+        self.profile.set_preference("browser.cache.disk.enable", False)
+        self.profile.set_preference("browser.cache.memory.enable", False)
+        self.profile.set_preference("browser.cache.offline.enable", False)
+        self.profile.set_preference("network.http.use-cache", False)
+        self.desiredCapabilities = DesiredCapabilities.FIREFOX.copy()
+        self.desiredCapabilities['firefox_profile'] = self.profile.encoded
+        self.options = Options()
+        self.options.add_argument("--headless")
 
-        driver = webdriver.Firefox(capabilities=desiredCapabilities, options=options)
+        self.driver = webdriver.Firefox(capabilities=self.desiredCapabilities, options=self.options)
 
-        driver.set_page_load_timeout(10)
-        wait = WebDriverWait(driver, 100, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+        self.driver.set_page_load_timeout(10)
+        self.wait = WebDriverWait(self.driver, 100, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
 #        url = "https://finance.yahoo.com/quote/" + self.stock_name + "?p=" + self.stock_name + "&.tsrc=fin-srch"
 
-        url = "https://finance.yahoo.com"
+        self.url = "https://finance.yahoo.com"
+        self.url_stock = "https://finance.yahoo.com/quote/"+stock.upper()+"?p="+stock.upper()
         while True:
             try:
-                driver.get(url)
-                driver.delete_all_cookies()
-                driver.implicitly_wait(10) # seconds
-                time.sleep(delay + 1)
+                self.driver.get(self.url)
+                self.driver.delete_all_cookies()
+                self.driver.implicitly_wait(10) # seconds
+                time.sleep(self.delay + 1)
                 print ("Yahoo finance Page is loaded")
-                if 'finance' in str(driver.current_url):
+                if 'finance' in str(self.driver.current_url):
                     break
             except TimeoutException:
                 pass
-
-        url_stock = "https://finance.yahoo.com/quote/"+stock.upper()+"?p="+stock.upper()
-    #     time.sleep(delay + 1)
-    # 
-    #     while True:
-    #         time.sleep(delay + 1)
-    #         try:
-    #             time.sleep(delay + 1)
-    #             driver.get(url_stock)
-    #             driver.implicitly_wait(10)
-    #             time.sleep(delay + 1)
-    # #            print (stock_name.upper(), str(driver.current_url))
-    #             if stock.upper() in str(driver.current_url):
-    #                 break
-    # 
-    #         except:
-    #              print ("Yahoo page slow, will reloop!")
-
-        #
+    def quit_driver(self):
+        self.driver.quit()
+        
+    def get_historical_data(self): 
         print ("Retrieving Historical Data ")
 
-        ts = datetime.datetime.strptime(str(date),"%Y-%m-%d")
+        self.ts = datetime.datetime.strptime(str(self.date),"%Y-%m-%d")
 
 #        tuple = element.timetuple()
-        timestamp = str(int(time.mktime(ts.timetuple())))
+        self.timestamp = str(int(time.mktime(self.ts.timetuple())))
         # print (timestamp)
         # print (date)
-        url_history = "https://finance.yahoo.com/quote/" + stock + "/history?period1=00&period2=" + timestamp +"&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
+        self.url_history = "https://finance.yahoo.com/quote/" + stock.upper() + "/history?period1=00&period2=" + self.timestamp +"&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
 #        url_history = "https://finance.yahoo.com/quote/" + stock + "/history?period1=00&period2=1626480000&interval=1d&filter=history&frequency=1d&includeAdjustedClose=true"
         while True:
-            time.sleep(delay + 1)
+            time.sleep(self.delay + 1)
             try:
-                time.sleep(delay + 1)
-                driver.get(url_history)
-                driver.implicitly_wait(5)
-                if stock.upper() in str(driver.current_url):
+                time.sleep(self.delay + 1)
+                self.driver.get(self.url_history)
+                self.driver.implicitly_wait(5)
+                if stock.upper() in str(self.driver.current_url):
                     break
             except:
                  print ("Yahoo page slow, will reloop!")
 
-        print ("clikc at Apply")
+#        print ("clikc at Apply")
         try:
-            elm = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Apply"]'))).click()
+            elm = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Apply"]'))).click()
         except TimeoutException:
             pass
 
-        time.sleep(delay + 3)
+        time.sleep(self.delay + 3)
 
-        a_elm = driver.find_element_by_xpath("//a[@class = 'Fl(end) Mt(3px) Cur(p)']")
-        print ("click at download link")
+        a_elm = self.driver.find_element_by_xpath("//a[@class = 'Fl(end) Mt(3px) Cur(p)']")
+#        print ("click at download link")
         a_elm.click()
-        time.sleep(delay + 3)
-        print ('\n')
+        time.sleep(self.delay + 3)
+#        print ('\n')
+        
+        weekno = datetime.datetime.today().weekday()
+        north_america = holidays.US()
+#                date = currentDateTime.date()
 
-#        if stock[1:] not in ['XAX', 'IXIC', 'DJI', 'GSPC', 'NYA']:
+        Day_of_today = self.date.strftime("%m-%d-%Y")
+#                print(Date_today)
+        if weekno < 5 and (Day_of_today not in north_america):
+            print("Saving current stock price.")
+            current_time = datetime.datetime.now()
+            open_time = current_time.replace(hour=9, minute=15, second=0, microsecond=0)
+#                    current_time = datetime.datetime.now().time()
+            # print(current_time)
+            if (current_time > open_time):
+                with open(downloadPath +"\\" + stock.upper() + '.csv', 'a') as file:
+#                       writer = csv.writer(file)
+                    last_row = ['\n',str(ts)[:10], ',', Open, ',', High, ',' , Low, ',', Current_price, ',', Current_price, ',', Volume_elm]
+                    file.writelines(last_row)
 
+ #        if stock[1:] not in ['XAX', 'IXIC', 'DJI', 'GSPC', 'NYA']:
+    def get_Summary_data(self): 
         print ("Display Summary Page")
 #        print(stock.upper(), str(driver.current_url))
 #        time.sleep(10000000)
         while True:
             try:
-                driver.get(url_stock)
-                driver.implicitly_wait(5)
-                time.sleep(delay + 1)
+                self.driver.get(self.url_stock)
+                self.driver.implicitly_wait(5)
+                time.sleep(self.delay + 1)
 
-                if stock.upper() in str(driver.current_url):
+                if stock.upper() in str(self.driver.current_url):
                     break
-
             except:
                 print ("Yahoo slow, will reloop!")
                 pass
 
-        print ('Current Price:   %s' % (driver.find_element_by_xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]').text))
+        print ('Current Price:   %s' % (self.driver.find_element_by_xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]').text))
 
         if self.stock_or_fund == 'stock':
             try:
-                elm = driver.find_element_by_xpath("//div[@class= 'Fw(b) Fl(end)--m Fz(s) C($primaryColor']").text
+                elm = self.driver.find_element_by_xpath("//div[@class= 'Fw(b) Fl(end)--m Fz(s) C($primaryColor']").text
                 print (elm, end = '\n')
             except Exception:
                 pass
 
-            Current_price = driver.find_element_by_xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]').text
+            Current_price = self.driver.find_element_by_xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]').text
 
-            Open = driver.find_element_by_xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[2]/td[2]/span').text
+            Open = self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[2]/td[2]/span').text
             print("Open =  %.2f" %float(Open))
 
-            Range_elm = driver.find_element_by_xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[5]/td[2]').text
+            Range_elm = self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[5]/td[2]').text
             Low, High  = Range_elm.split(' - ')[0], Range_elm.split(' - ')[1]
             print ("LOW = %s, HIGH = %s" %(Low, High))
 
-            Volume_elm = driver.find_element_by_xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[7]/td[2]/span').text
+            Volume_elm = self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[7]/td[2]/span').text
             Volume_elm = Volume_elm.replace(',', '')
             print("Volume =  %d" %int(Volume_elm))
 
-            weekno = datetime.datetime.today().weekday()
-            north_america = holidays.US()
-#                date = currentDateTime.date()
-
-            Day_of_today = date.strftime("%m-%d-%Y")
-#                print(Date_today)
-            if weekno < 5 and (Day_of_today not in north_america):
-                current_time = datetime.datetime.now()
-                open_time = current_time.replace(hour=9, minute=15, second=0, microsecond=0)
-#                    current_time = datetime.datetime.now().time()
-                # print(current_time)
-                if (current_time > open_time):
-#                    if (int(str(current_time.hour)) > 8):
-                # with open(downloadPath +"\\" + stock.upper() + '.csv', 'r') as f:
-                #     for line in f:
-                #         pass
-                #     last_line = line
-                # last_date = last_line.split(',')
-                # if last_date != str(ts)[:10]:
-                    
-                
-                    with open(downloadPath +"\\" + stock.upper() + '.csv', 'a') as file:
-#                       writer = csv.writer(file)
-                        last_row = ['\n',str(ts)[:10], ',', Open, ',', High, ',' , Low, ',', Current_price, ',', Current_price, ',', Volume_elm]
-                        file.writelines(last_row)
 
 #               Search Beta Value
-            table_elm = driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody')
+            table_elm = self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody')
             list_elm = table_elm.find_elements_by_xpath('//*/tr[2]')
 
             for elm in list_elm:
@@ -243,25 +221,25 @@ class get_historical_data():
 #                Print bullish or bearish
             try:
 
-                 print ("bullish or bearish: ==> %s" %(driver.find_element_by_xpath('//html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[1]/div/div[3]/div[2]/div[1]/span[1]/span').text))
+                 print ("bullish or bearish: ==> %s" %(self.driver.find_element_by_xpath('//html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[1]/div/div[3]/div[2]/div[1]/span[1]/span').text))
             except:
                 print("bullish or bearish not found")
                 
-            EPS =  driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[4]/td[2]/span').text
+            EPS =  self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[4]/td[2]/span').text
             print ("EPS ( > 1 is better ) = %s" %EPS)
             
-            PE_Rato = driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[3]/td[2]/span').text
+            PE_Rato = self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[3]/td[2]/span').text
             print ("PE_Rato ( Smaller is better ) = %s" %PE_Rato)
         else:
 
-            table_elm = driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody')
+            table_elm = self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody')
             list_elm = table_elm.find_elements_by_xpath('//*/tr[6]')
 
             for elm in list_elm:
                 if 'Beta' in elm.text:
                    print (elm.text)
 
-            table_elm = driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody')
+            table_elm = self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody')
             list_elm = table_elm.find_elements_by_xpath('//*/tr[2]')
             for elm in list_elm:
 
@@ -269,11 +247,10 @@ class get_historical_data():
                    print (elm.text)
 
             if stock_or_fund == 'ETF':
-                print (driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[2]/td[1]/span').text, end ='   ')
-                print (driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[2]/td[2]/span').text)
+                print (self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[2]/td[1]/span').text, end ='   ')
+                print (self.driver.find_element_by_xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[2]/td[2]/span').text)
 
         print ('\n' *3)
-        driver.quit()
 
 def main():
     global downloadPath
@@ -284,9 +261,7 @@ def main():
          pass
     time.sleep(2)
     os.mkdir(downloadPath)
-
     sys.stdout = Logger()
-
     now_time = datetime.datetime.now().time()
     print("\nTime: ", now_time, "\n")
 
@@ -318,8 +293,13 @@ def main():
 #        print (stock.group())
         stock = stock.group().rstrip().rstrip(')').lstrip('(')
 #        get_stock_data = get_historical_data(stock.group().rstrip().rstrip(')').lstrip('('),  downloadPath, stock_or_fund)
-        get_stock_data = get_historical_data(stock_or_fund)
+        get_history_data = get_data(stock_or_fund)
 
+#        get_history_data.get_historical_data()
+
+        get_history_data.get_Summary_data()
+
+        get_history_data.quit_driver()
 if __name__ == "__main__":
 
     main()
