0382ebc30c48cbae97516845abf4abcc035b8002
weipanchang <weipanchang@hotmail.com>
2024-06-09 01:09:32 -0700

    scrape with sql

--- 1-stock_batch_data_tipranks_1.py
+++ 1-stock_batch_data_tipranks_1.py
@@ -153,6 +153,23 @@ def main():
             return False
         return True
     
+    def extract_price(s, n, sub2):
+
+        idx2 = s.index(sub2)
+        return(s[3: idx2])
+    
+    def extract_price_3(s, sub1, sub2):
+    
+        idx1 = s.index(sub1)
+        idx2 = s.index(sub2)
+        return(s[idx1 + len(sub1): idx2])
+    
+    
+    # def extract_price_2(s, sub1):
+    # 
+    #     idx1 = s.index(sub1)
+    #     return(s[idx1 + len(sub1):])
+    
     def fetch_Stock_Name(stock_Dictionary):
         stock_fund_names =  [line for line in open("STOCK.txt", "r")]
         for stock_fund_name in stock_fund_names:
@@ -193,49 +210,36 @@ def main():
     #driver.find_element("xpath","//input[@name = 'email'").click()
     email_box = driver.find_element(By.XPATH,"//input[contains(@class, 'w12 py4 px3 radiimedium')]")
     email_box.click()
-    email_box.send_keys("weipanchang@gmx.com")
+    email_box.send_keys("weipanchang@mail.com")
     
     password_box = driver.find_element(By.XPATH,"//input[contains(@type, 'password')]")
     password_box.click()
-    password_box.send_keys("Abcde12345")
+    password_box.send_keys("abcde12345")
     
-    signin_button = driver.find_element(By.XPATH,"//button[contains(@class, 'colorwhite w12 radiiround displayflex bgorange-light hoverBgorange h_px1 flexrcc fontSize6 fontWeightsemibold aligncenter w_px6 my4 mobile_fontSize6 mobile_py3 mobile_h_pxauto mobile_mt5')]")
+    #signin_button = driver.find_element(By.XPATH,"colorwhite w12 radiiround displayflex bgorange-light hoverBgorange h_px1 flexrcc fontSize6 fontWeightsemibold aligncenter w_px6 mt4 mb3 mobile_fontSize6 mobile_py3 mobile_h_pxauto mobile_mt5')]")
+    signin_button = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[5]/div[1]/div[2]/form/button")
+    
     signin_button.click()
     
     time.sleep(10)   
     #actions = ActionChains(driver)
     webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-    #time.sleep(10)
-    #os.system("PAUSE")
-    
-    # mycookie = driver.get_cookies()
-    # print(mycookie)
-    # try:
-#    stock_table = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[5]/div[2]/div[3]/div[2]/div[4]/div[1]/div[2]/table")
-#        print("found stock_table")
-    # except:
-    #     print("not found")
-#    menu_bar = driver.find_elements(By.XPATH,"/html/body/div[1]/div[2]/div[1]/div[2]/div/div[1]/div[3]/div/div/div/div/div/div[4]/button/div/div")
-    #profile_button = menu_bar.find_element(By.XPATH, "//span[text()='Portfolio']")
-
     
     time.sleep(1)
-    sys.stdout = Logger()
+
     
     webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
     time.sleep(3)
 
-    # try:
-    stock_table = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[5]/div[2]/div[3]/div[2]/div[4]/div[1]/div[2]/table")
-        # print("found stock_table")
-    # except:
-        # print("not found")
-        
+    # # try:
+    # stock_table = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[5]/div[2]/div[3]/div[2]/div[4]/div[1]/div[2]/table")
+    #     # print("found stock_table")
+    # # except:
+    #     # print("not found")
 
 
-    #os.system("PAUSE")
     #stock_input_box =  driver.find_element(By.XPATH,"//input[@id='react-select-2-input']")
-                                    
+    sys.stdout = Logger()                                    
     fetch_Stock_Name(stock_Dictionary:={})
     for stock in stock_Dictionary.keys():
     
@@ -250,22 +254,30 @@ def main():
         time.sleep(15)
         stock_input_box = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='react-select-2-input']")))
         stock_input_box.click()
-        # while True:
-        # try:
-        #     stock_input_box.click()
-        #     break
-        # except
-        #   <div id="gray4093">
-        #     <div id="gray77">
+        time.sleep(1)
+        stock_input_box.clear()
         time.sleep(1)
         webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
         time.sleep(1)
+
         stock_input_box.send_keys(stock)
+        time.sleep(3)
         stock_input_box.send_keys(Keys.ENTER)
 
+        # stock_input_box = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='react-select-2-input']")))
+        # stock_input_box.click()
+
+        time.sleep(1)
+        driver.refresh()
+        # stock_input_box.send_keys(stock)
+        # time.sleep(6)
+        # stock_input_box.send_keys(Keys.ENTER)
+
+        # time.sleep(1)
+        # stock_input_box.send_keys(Keys.ENTER)
         webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-        time.sleep(3)
-        
+        time.sleep(6)
+#        os.system("PAUSE")
         # while True:
         #     try:
         #         print (('Current Price:   %s') % (driver.find_element("xpath",'//span[@class= "transitioncolor"]')).text[1:])
@@ -280,51 +292,32 @@ def main():
         #     print("After Hours:     %s\n" % (driver.find_element("xpath",'//span[@class = "mr3  transitioncolor"]').text[1:]))
         # 
         # # try:
-        #     print("1y Target High Est = %s\n" % (driver.find_element("xpath",'//span[@class="colorpale  ml3 mobile_fontSize7 laptop_ml0"]').text[1:]))
-        # except:
-        #     pass
-        
-        while True:
-            try:
-#                self.driver.implicitly_wait(3) # seconds
-                print("1y Target Est = %s\n" % (driver.find_element("xpath",'//span[@class="colorgray-1  ml3 mobile_fontSize7 laptop_ml0"]').text[1:]))
-                break
-            except:
-                webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-                time.sleep(1)                
-                pass
+        try:
+            driver.find_element(By.XPATH,'//*[@id="tr-stock-page-content"]')
             
-        # try:
-        #     print("1y Target Low Est = %s\n" % (driver.find_element("xpath",'//span[@class="colorpurple-dark  ml3 mobile_fontSize7 laptop_ml0"]').text[1:]))
-        # except:
-        #     pass
-        
-#         while True:
-#             try:
-# #                self.driver.implicitly_wait(3) # seconds
-        time.sleep(1)                
-#        print("Recommedation:    %s\n" % (driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[4]/div[3]/div[1]/div[1]/div[5]/div[1]/div[2]/div[3]/div[2]/div/span").text))
-#        /html/body/div[1]/div[2]/div[4]/div[3]/div[1]/div[1]/div[5]/div[1]/div[2]/div[3]/div[2]/div/span
-#                 break
-#             except:
-#                 if check_exists_by_xpath('button[@class="colorwhite hoverBgorange mt4 bgorange-light radiilarge1 aligncenter fontSize5 border1 py2 w_px200 mobile_fontSize6 mobile_w_px3"]'):
-#                     driver.find_element("xpath", 'button[@class="colorwhite hoverBgorange mt4 bgorange-light radiilarge1 aligncenter fontSize5 border1 py2 w_px200 mobile_fontSize6 mobile_w_px3"]').click()
-#                 if check_exists_by_xpath('button[@class="colorwhite hoverBgorange mt4 bgorange-light radiilarge1 aligncenter fontSize5 border1 py2 w_px200 mobile_fontSize6 mobile_w_px3"]'):
-#                     driver.find_element("xpath", 'button[@class="a__sc-np32r2-0 iTPDzx"]').click()
-#                 if check_exists_by_xpath('div[@class="a__sc-np32r2-0 iTPDzx using-keyboard"]'):
-#                     driver.find_element("xpath", 'div[@class="a__sc-np32r2-0 iTPDzx using-keyboard"]').click()                    
-#                     
-#                 pass  
-        
-    #    os.system("PAUSE")
-    #  
-    # # Wait for page to load
-    # while driver.execute_script("return document.readyState") != "complete":
-    #     pass
-    #  
-    # # Take screenshot
-    # driver.save_screenshot("opensea.png")
-     
+#           /html/body/div[2]/div[2]/div[4]/div[3]/div[1]/div[1]/div[5]/div[2]/div[2]/div[3]/div[2]/div/div[1]/div[1]
+            frame = driver.find_element(By.XPATH,'//*[@id="tr-stock-page-content"]')
+            
+#            print("Found")
+        except NoSuchElementException:
+            print("Frame NOT Found")
+#
+        element = frame.find_element(By.XPATH,'//div[@class="flexccc    mt3 displayflex colorpale shrink0 lineHeight2 fontSize2 ml2 ipad_fontSize3"]')
+        element.click()
+        value = str((element.text).encode('utf8'))
+#       print(value)
+        target  = extract_price_3(value, "$","\\n\\xe2")
+
+        print( "1y Target Est = %s\n" %(target))
+#            print("Found")
+        # except NoSuchElementException:
+        #     print("NOT Found")
+#        return True
+#        print("1y Target High Est = %s\n" % (driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div[4]/div[3]/div[1]/div[1]/div[5]/div[2]/div[2]/div[3]/div[2]/div/div[1]/div[1]')))
+#       os.system("PAUSE")
+
+ #      time.sleep(1)                
+
     # Close browser
     driver.quit()
 
