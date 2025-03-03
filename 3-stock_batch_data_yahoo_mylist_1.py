010564d9e6795ba08243f827ef325a7c975937eb
weipanchang <weipanchang@hotmail.com>
2025-03-03 09:31:47 -0800

    fix profile issue

---
+++ 3-stock_batch_data_yahoo_mylist_1.py
@@ -0,0 +1,338 @@
+#!/usr/bin/env python
+"""
+Firefox version: 73.0 (64-bit)
+"""
+#import xml.etree.ElementTree as ET
+#import urllib2
+import requests, urllib3, sys
+import re
+from path import Path
+import os
+import holidays
+import shutil
+import random
+import logging
+from yahoo_fin import stock_info as si
+import yfinance as yf
+from openpyxl import load_workbook, Workbook
+
+# from bs4 import BeautifulSoup
+# import unittest
+from selenium import webdriver
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.common.exceptions import NoSuchElementException
+import time
+import datetime
+from datetime import date
+# import sys
+#from selenium import webdriver
+downloadPath = os.path.expanduser( '~' ) + "\\Documents\\Python Scripts\\data"
+Path(os.path.expanduser( '~' ) + "\\Documents\\Python Scripts").chdir()
+#eXCEL_File = os.path.expanduser( '~' ) + "\\Documents\\Python Scripts\\Stock_2.xlsx"
+        
+stock = ""
+
+class Logger(object):
+
+    def __init__(self):
+#        global downloadPath
+        global stock
+        today = date.today()
+        #d1 = today.strftime("%m%d%Y")
+        self.terminal = sys.stdout
+        self.log = open(downloadPath +"\\Summary_Report__From_Yahoo_"+ today.strftime("%m%d%Y") + ".txt" , "a+")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass
+
+class init_webdriver():
+    def __init__(self):
+#        global downloadPath
+        global stock
+#        stock_name = stock
+        print ("")
+
+        self.delay = 0
+        self.currentDateTime = datetime.datetime.now()
+        self.date = self.currentDateTime.date()
+        self.options = Options()
+        self.options.set_preference("browser.download.folderList", 2)
+        self.options.set_preference("browser.download.manager.showWhenStarting", False)
+        self.options.set_preference("browser.download.dir", downloadPath)
+        self.options.set_preference("browser.helperApps.neverAsk.openFile", "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+        self.options.set_preference("browser.helperApps.neverAsk.saveToDisk", "text/csv,application/x-msexcel,application/excel,application/x-excel,application/vnd.ms-excel,image/png,image/jpeg,text/html,text/plain,application/msword,application/xml")
+        self.options.set_preference("browser.helperApps.alwaysAsk.force", False)
+        self.options.set_preference("browser.download.manager.alertOnEXEOpen", False)
+        self.options.set_preference("browser.download.manager.focusWhenStarting", False)
+        self.options.set_preference("browser.download.manager.useWindow", False)
+        self.options.set_preference("browser.download.manager.showAlertOnComplete", False)
+        self.options.set_preference("browser.download.manager.closeWhenDone", False)
+        self.options.set_preference("browser.cache.disk.enable", False)
+        self.options.set_preference("browser.cache.memory.enable", False)
+        self.options.set_preference("browser.cache.offline.enable", False)
+        self.options.set_preference("network.http.use-cache", False)
+        self.desiredCapabilities = DesiredCapabilities.FIREFOX.copy()
+        self.service = Service(r"'~'+'\.cache\selenium\geckodriver\win64\0.36.0'")
+#        self.service = Service(r'C:\Users\William Chang\.cache\selenium\geckodriver\win64\0.36.0')
+#        self.capabilities = DesiredCapabilities.FIREFOX.copy()
+#        self.desiredCapabilities['firefox_profile'] = self.profile.encoded
+        self.options = Options()
+        
+#        self.driver = webdriver.Firefox(capabilities=self.desiredCapabilities, options=self.options)
+
+        #self.driver.set_page_load_timeout(50)
+        #wait = WebDriverWait(self.driver, 200, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+        
+        #run in headless mode
+        #self.options.add_argument("--headless")
+        
+        # disable the AutomationControlled feature of Blink rendering engine
+        self.options.add_argument('--disable-blink-features=AutomationControlled')
+        #  
+        # disable pop-up blocking
+        self.options.add_argument('--disable-popup-blocking')
+        #  
+        # # start the browser window in maximized mode
+        self.options.add_argument('--start-maximized')
+        #  
+        # disable extensions
+        self.options.add_argument('--disable-extensions')
+        #  
+        # disable sandbox mode
+        self.options.add_argument('--no-sandbox')
+        #  
+        # disable shared memory usage
+        self.options.add_argument('--disable-dev-shm-usage')
+        
+        # Step 3: Rotate user agents 
+        user_agents = [
+            # Add your list of user agents here
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0"
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        ]
+
+        # select random user agent
+        user_agent = random.choice(user_agents)
+#        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0"
+        # pass in selected user agent as an argument
+        self.options.add_argument(f'user-agent={user_agent}')
+    def driver_init(self):
+        self.driver = Firefox(service=self.service, options=self.options)
+#        self.driver = webdriver.Firefox(capabilities=self.desiredCapabilities, options=self.options)
+        self.driver.set_page_load_timeout(100)
+        return(self.driver)
+
+        
+def main():
+#    global downloadPath
+    global stock
+    try:
+        shutil.rmtree(downloadPath)
+    except:
+         pass
+    time.sleep(2)
+    try:
+        os.mkdir(downloadPath)
+    except:
+        pass
+
+    now_time = datetime.datetime.now().time()
+    print("\nTime: ", now_time, "\n")
+
+    logging.basicConfig(level=logging.INFO)
+    driver = init_webdriver().driver_init()
+    driver.get('https://finance.yahoo.com/')
+    time.sleep(1)
+    driver.minimize_window()
+    def check_exists_by_css_selector(driver, css_selector):
+        try:
+            driver.find_element(By.CSS_SELECTOR,css_selector)
+        except NoSuchElementException:
+            return False
+        return True
+    def check_exists_by_classname(driver,classname):
+        try:
+            driver.find_element(By.CLASS_NAME,classname)
+        except NoSuchElementException:
+            return False
+        return True
+    
+    def check_exists_by_xpath(driver,xpath):
+        try:
+            driver.find_element(By.XPATH,xpath)
+        except NoSuchElementException:
+            return False
+        return True
+    def check_exists_by_tag(driver,tag_name):
+        try:
+            driver.find_element(By.TAG_NAME,tag_name)
+        except NoSuchElementException:
+            return False
+        return True
+        
+    def fetch_Stock_Name(stock_Dictionary):
+        stock_fund_names =  [line for line in open("STOCK.txt", "r")]
+        for stock_fund_name in stock_fund_names:
+            if len(stock_fund_name) < 2 or "IGNOR" in stock_fund_name :
+                continue
+
+            stock = re.search(r'(\(\^\w+\))', stock_fund_name)
+            if stock is None:
+                stock = re.search('\(\w+\)', stock_fund_name)
+                msft_ticket = re.search('\[\w+\]', stock_fund_name)
+
+            is_stock =  re.search("ETF|Fund",stock_fund_name)
+#            print is_stock
+            if is_stock:
+                if 'ETF' in stock_fund_name:
+                    stock_or_fund =  'ETF'
+                else:
+                    stock_or_fund = 'Fund'
+            else:
+                stock_or_fund ='STOCK'
+            # print(stock_or_fund)
+            stock = stock.group().rstrip().rstrip(')').lstrip('(')
+            msft_ticket = msft_ticket.group().rstrip().rstrip(']').lstrip('[')
+            stock_Dictionary[stock] = [stock_fund_name.rstrip()[:-9]]
+            
+            stock_Dictionary[stock].append(stock_or_fund)
+            stock_Dictionary[stock].append(msft_ticket)
+    sys.stdout = Logger()
+    fetch_Stock_Name(stock_Dictionary:={})
+    for stock in stock_Dictionary.keys():
+
+#        sys.stdout = Logger()
+        print("\n")
+        print (("=") * len("Processing " + stock_Dictionary[stock][0] +" data"))
+        print ("Processing " + stock_Dictionary[stock][0] +" data")
+        print (("=") * len("Processing " + stock_Dictionary[stock][0] +" data"))
+        print("\n")
+        
+        url_stock = "https://finance.yahoo.com/quote/"+ stock.upper()
+        driver.get(url_stock)
+ #       print(url_stock)
+        driver.implicitly_wait(1)
+#       delay = 1
+        while True:
+            try:
+                driver.get(url_stock)
+                driver.implicitly_wait(2)
+                time.sleep(1)
+                print(str(driver.current_url))
+                if stock.upper() in str(driver.current_url):
+                    break
+            except:
+                print ("Yahoo page slow, will reloop!", end=" ")
+                pass
+        time.sleep(3)
+#         try:
+#             print ('Current Price:   %s' % (driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/fin-streamer[1]/span').text))
+#         except:
+#             print ('Current Price:   %s' % (driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/div[1]/section/div[1]/div[1]/span').text))
+# #                                                                         /html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/div[1]/span
+
+        if check_exists_by_xpath(driver, "/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/fin-streamer[1]/span"):
+            print ('Current Price:   %s' % (driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/fin-streamer[1]/span').text))
+        if check_exists_by_xpath(driver, "/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/div[1]/section/div[1]/div[1]/span"):
+            print ('Current Price:   %s' % (driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/div[1]/section/div[1]/div[1]/span').text))
+        if check_exists_by_xpath(driver, "/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/div[1]/span"):
+            print ('Current Price:   %s' % (driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/div[1]/span').text))            
+
+        if check_exists_by_xpath(driver, "//fin-streamer[contains(@data-field,'postMarketPrice')]"):
+            print("After Hours:     %s\n" % (driver.find_element(By.XPATH,"//fin-streamer[contains(@data-field,'postMarketPrice')]").text))
+        if check_exists_by_xpath(driver, "//fin-streamer[@data-field='regularMarketPreviousClose']"):
+            print("Previous   :     %s\n" % (driver.find_element(By.XPATH,"//fin-streamer[@data-field='regularMarketPreviousClose']").text))
+
+        try:
+             Open = driver.find_element(By.XPATH,"//fin-streamer[@data-field='regularMarketOpen']").text
+        
+        except:
+              Open = driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[2]/span[2]/fin-streamer').text
+        print("Open =  %.2f" %float(Open.replace(',','')))
+        print()
+
+        try:
+            Range_elm = driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[5]/span[2]/fin-streamer').text
+        except NoSuchElementException:
+            Range_elm = driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[5]/span[2]/fin-streamer').text
+        Low, High  = Range_elm.split(' - ')[0], Range_elm.split(' - ')[1]
+        print ("LOW = %s, HIGH = %s\n" %(Low, High))
+
+        try: 
+            Ex_Dividend = driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[15]/span[2]').text
+            print ("Ex-Dividend Date = %s\n" %Ex_Dividend)
+        except NoSuchElementException:
+            Ex_Dividend = driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[15]/span[2]').text
+            print ("Ex-Dividend Date = %s\n" %Ex_Dividend)
+
+        try:
+            beta = driver.find_element(By.XPATH,"/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[10]/span[2]").text
+        except:
+            beta = driver.find_element(By.XPATH,"/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[10]/span[2]").text
+        
+        print( "Beta (5Y Monthly) = ", beta)
+        try:
+            target = driver.find_element(By.XPATH,"/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[16]/span[2]/fin-streamer").text
+        except:
+            target = driver.find_element(By.XPATH,"/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[16]/span[2]/fin-streamer").text            
+        print( "\n1y Target Est =========> ", target)
+        print("")
+
+        try:
+            EPS =  driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[12]/span[2]/fin-streamer').text
+        except NoSuchElementException:
+            EPS =  driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[12]/span[2]/fin-streamer').text
+        
+        print ("EPS ( > 1 is better ) ====       %s" %EPS)
+# 
+        try:
+            PE_Ratio = driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[11]/span[2]/fin-streamer').text
+        except NoSuchElementException:
+            PE_Ratio = driver.find_element(By.XPATH,'/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[11]/span[2]/fin-streamer').text
+        print ("PE_Ratio ( Smaller is better ) = %s" %PE_Ratio)
+#         os.system("PAUSE")                             
+           
+        try:
+            Volume =  driver.find_element(By.XPATH, '/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[7]/span[2]/fin-streamer').text.replace(",","")
+        except:
+            Volume =  driver.find_element(By.XPATH, '/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[7]/span[2]/fin-streamer').text.replace(",","")
+          
+        try:
+            Avg_Volume =  driver.find_element(By.XPATH, '/html/body/div[2]/main/section/section/section/article/div[3]/ul/li[8]/span[2]/fin-streamer').text.replace(",","")
+        except:
+            Avg_Volume =  driver.find_element(By.XPATH, '/html/body/div[2]/main/section/section/section/article/div[2]/ul/li[8]/span[2]/fin-streamer').text.replace(",","")
+
+        try:
+            print ("\nVolume over Average = %s\n" %round(float(Volume)/float(Avg_Volume),2))
+        except:
+            pass
+        print ("EOT")
+        print ('\n' *2)
+
+
+    driver.quit()
+        
+if __name__ == "__main__":
+    main()
