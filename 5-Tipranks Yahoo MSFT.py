70c004801b43aa7f26467607062633be0ddb7931
weipanchang <weipanchang@hotmail.com>
2025-04-16 20:56:24 -0700

    handle pdf

---
+++ 5-Tipranks Yahoo MSFT.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+import subprocess
+from path import Path
+import time
+import re
+import datetime
+import shutil
+import sys
+from datetime import date
+#from selenium_stealth import stealth
+import random
+import os
+
+Path(os.path.expanduser( '~' ) + "\\Documents\\Python Scripts").chdir()
+
+vpn_server_names =  [line for line in open("vpn_list_1.txt", "r")]
+#n =  len(vpn_server_names)
+random_server_list=  random.sample(vpn_server_names, 4)
+
+print (random_server_list)
+out = open(os.path.expanduser( '~' ) + "\\Documents\\Python Scripts\\"+"random_server_list.txt", "wb")
+out.write(str(random_server_list).encode()) # write text of page
+out.close()
+
+user_pass_pairs =  [line for line in open("UserName_Password.txt", "r")]
+#n =  len(vpn_server_names)
+user_pass_pairs=  random.sample(user_pass_pairs, 4)
+print (user_pass_pairs)
+out = open(os.path.expanduser( '~' ) + "\\Documents\\Python Scripts\\"+"user_pass_pairs.txt", "wb")
+out.write(str(user_pass_pairs).encode()) # write text of page
+out.close()
+
+
+def connect_pia_vpn(region):
+    try:
+        # Command to connect to PIA VPN
+        # command = f'"C:\Program Files\Private Internet Access\\piactl.exe" resetsettings'
+        # subprocess.run(command, shell=True, check=True)
+        command = f'"C:\Program Files\Private Internet Access\\piactl.exe" set region {region}'
+        subprocess.run(command, shell=True, check=True)
+        command = f'"C:\Program Files\Private Internet Access\\piactl.exe" connect'
+        subprocess.run(command, shell=True, check=True)
+        command = f'"C:\Program Files\Private Internet Access\\piactl.exe" get region'
+        subprocess.run(command, shell=True, check=True)
+        print(f"Connected to PIA VPN in {region}")
+    #     command = f'"C:\Program Files\Private Internet Access\\piactl.exe" disconnect'
+    #     subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to connect to PIA VPN: {e}")
+        
+def dis_connect_pia_vpn():
+    try:
+        command = f'"C:\Program Files\Private Internet Access\\piactl.exe" disconnect'
+        subprocess.run(command, shell=True, check=True)
+        print(f"Disconnected to PIA VPN")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to disconnect to PIA VPN: {e}")
+    
+# Replace 'US' with your desired region
+
+for i in range(4):
+    
+    region = random_server_list[i]
+    connect_pia_vpn(region)
+    
+    user,password = user_pass_pairs[i].split()
+#    user,password = user_pass_pair
+    print(user,password)
+    k = i + 1
+    command = f'"5-stock_batch_data_tipranks_{k}.exe" -l {user} {password}'
+    os.system(command)
+    # command = f'"1-stock_batch_data_tipranks_{k}.py" -l {user} {password}'
+    # subprocess.run(command, shell=True, check=True)
+#    os.system("pause")
+    
+dis_connect_pia_vpn()
+# 
+command = f'"3-stock_batch_data_msft_mylist_1.exe"'
+os.system(command)
+#subprocess.run(command, shell=True, check=True)
+
+command = f'"3-stock_batch_data_yahoo_mylist_1.exe"'
+os.system(command)
+#subprocess.run(command, shell=True, check=True)
+
+    
+    
+    
+    
---
+++ 5-stock_analysis_xfer.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+import requests
+import time
+import datetime
+from datetime import date
+from datetime import timedelta
+from path import Path
+import os
+import sys
+import shutil
+import re
+import logging
+
+dataPath = os.path.expanduser( '~' ) + "\\Documents\\Python Scripts\\Stock_Analysis\\"                                     
+downloadPath = os.path.expanduser( '~' ) + '\Downloads\\'
+sTock_Analysis_data_file_name = "Stock Watchlist & Portfolio Tracker"
+sTock_Analysis_pdf_file = sTock_Analysis_data_file_name + ".pdf"
+sTock_Analysis_data_file = sTock_Analysis_data_file_name + ".txt"
+source = downloadPath + sTock_Analysis_data_file
+
+class Logger(object):
+
+    def __init__(self):
+        global downloadPath
+        today = date.today()
+
+        self.terminal = sys.stdout
+        self.log = open(dataPath +"\\Summary_Report_From_Stock_Analysis_"+ today.strftime("%m%d%Y") + ".txt" , "w")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass
+     
+def main():
+    time.sleep(5)
+    def extract_data(data_list):
+        line_from_Stock_Analysis =""
+        with open(dataPath+sTock_Analysis_data_file) as Stock_Analysis:
+            try:
+                line_from_Stock_Analysis = Stock_Analysis.readline()
+            except:
+                pass
+            while "Next Year" not in line_from_Stock_Analysis:
+
+                line_from_Stock_Analysis = Stock_Analysis.readline()
+
+                # print(line_from_Stock_Analysis)
+                # os.system("pause")
+#                    continue
+            # os.system("pause")
+            
+            while True:
+#                line_from_Stock_Analysis = Stock_Analysis.readline()
+#                if "Watchlist Averages" not in line_from_Stock_Analysis:
+                # try:
+                line_from_Stock_Analysis = Stock_Analysis.readline()
+                # except:
+                #     pass
+                if "Watchlist Averages" not in line_from_Stock_Analysis:
+                    if len(line_from_Stock_Analysis)  != 0:
+                        elements = line_from_Stock_Analysis.split()
+                        if len(elements)  > 5:
+#                            print(elements)
+                            data_list[elements[0]] = [elements[3].replace(",", "")]
+                            data_list[elements[0]].append(elements[4])
+                else:
+                    break
+    
+    def fetch_Stock_Name(stock_Dictionary):
+        stock_fund_names =  [line for line in open("STOCK.txt", "r")]
+        for stock_fund_name in stock_fund_names:
+            if len(stock_fund_name) < 2 or "IGNOR" in stock_fund_name :
+                continue
+    
+            stock = re.search(r'(\(\^\w+\))', stock_fund_name)
+            if stock is None:
+                stock = re.search('\(\w+\)', stock_fund_name)
+                msft_ticket = re.search('\[\w+\]', stock_fund_name)
+    
+            is_stock =  re.search("ETF|Fund",stock_fund_name)
+    #            print is_stock
+            if is_stock:
+                if 'ETF' in stock_fund_name:
+                    stock_or_fund =  'ETF'
+                else:
+                    stock_or_fund = 'Fund'
+            else:
+                stock_or_fund ='STOCK'
+    
+            stock = stock.group().rstrip().rstrip(')').lstrip('(')
+            msft_ticket = msft_ticket.group().rstrip().rstrip(']').lstrip('[')
+            stock_Dictionary[stock] = [stock_fund_name.rstrip()[:-9]]
+            
+            stock_Dictionary[stock].append(stock_or_fund)
+            stock_Dictionary[stock].append(msft_ticket)
+            
+    try:
+        shutil.rmtree(dataPath)
+    except:
+         pass
+    time.sleep(2)
+    try:
+        os.mkdir(dataPath)
+    except:
+        pass            
+#    source = "C:\\Users\\William Chang\\Downloads\\Stock Watchlist & Portfolio Tracker.txt"
+#    destination = "D:\Pycharm projects\gfg\Test\A"
+    shutil.move(source, dataPath)            
+    fetch_Stock_Name(stock_Dictionary:={})
+
+ #  print(stock_Dictionary)
+    sys.stdout = Logger()
+#    os.system("pause")    
+
+    extract_data(data_list:={})
+#   print (data_list)
+#    os.system("pause")
+    for stock in stock_Dictionary.keys():
+        
+        print("\n")
+        print (("=") * len("Processing " + stock_Dictionary[stock][0] +" data"))
+        print ("Processing " + stock_Dictionary[stock][0] +" data")
+        print (("=") * len("Processing " + stock_Dictionary[stock][0] +" data"), end="\n")
+        
+        print ("\n1y Target Est = %s\n" % (data_list[stock][0]))
+        print ("\nPrice Target Upside Percent = %s\n" % (data_list[stock][1]))
+ 
+if __name__ == '__main__':
+    
+    main()
